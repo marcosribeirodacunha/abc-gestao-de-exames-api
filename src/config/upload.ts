0aeae5e4c1f98090ca18e1ad01d03642f1e84333
marcosribeirodacunha <mrcmarcos100@gmail.com>
2020-11-29 11:21:34 -0300

    Add upload to IBM Cloud Object Storage

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,14 +1,17 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import crypto from 'crypto';
 import { Request } from 'express';
+import ibm from 'ibm-cos-sdk';
 import multer from 'multer';
+import multerS3 from 'multer-s3';
 import path from 'path';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
-export default {
-  directory: tmpFolder,
+type storageType = { [key: string]: multer.StorageEngine };
 
-  storage: multer.diskStorage({
+const storageTypes: storageType = {
+  local: multer.diskStorage({
     destination: tmpFolder,
     filename: (req, file, cb) => {
       const fileHash = crypto.randomBytes(10).toString('hex');
@@ -17,6 +20,30 @@ export default {
     },
   }),
 
+  cos: multerS3({
+    s3: new ibm.S3({
+      endpoint: process.env.IBM_ENDPOINT,
+      apiKeyId: process.env.IBM_API_KEY_ID,
+      ibmAuthEndpoint: process.env.IBM_AUTH_ENDPOINT,
+      serviceInstanceId: process.env.IBM_SERVICE_INSTANCE_ID,
+    }),
+    bucket: process.env.IBM_BUCKET!,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (req, file, cb) => {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+      file.key = `${fileHash}-${file.originalname}`;
+      cb(null, file.key);
+    },
+  }),
+};
+
+export default {
+  directory: tmpFolder,
+  storage: storageTypes[process.env.STORAGE_TYPE || 'local'],
+  limits: {
+    fileSize: 3 * 1024 * 1024,
+  },
   fileFilter: (
     req: Request,
     file: Express.Multer.File,
--- src/helpers/deletePhoto.ts
+++ src/helpers/deletePhoto.ts
@@ -1,6 +1,23 @@
 import fs from 'fs';
+import ibm from 'ibm-cos-sdk';
 import path from 'path';
 
 export default function deletePhoto(key: string) {
+  if (process.env.STORAGE_TYPE === 'cos') {
+    const cos = new ibm.S3({
+      endpoint: process.env.IBM_ENDPOINT,
+      apiKeyId: process.env.IBM_API_KEY_ID,
+      ibmAuthEndpoint: process.env.IBM_AUTH_ENDPOINT,
+      serviceInstanceId: process.env.IBM_SERVICE_INSTANCE_ID,
+    });
+
+    return cos
+      .deleteObject({
+        Bucket: process.env.IBM_BUCKET!,
+        Key: key,
+      })
+      .promise();
+  }
+
   return fs.promises.unlink(path.resolve(__dirname, '..', '..', 'tmp', key));
 }
